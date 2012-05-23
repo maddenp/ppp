b94a75bdcf4800f35c6ff7015c753a20bb83f4be
Paul Madden <paul.a.madden@noaa.gov>
2012-05-23 09:58:04 -0600

    upcase instead of downcase (f90 sect. 3.1.1)

--- normalize_nodes.rb
+++ normalize_nodes.rb
@@ -15,7 +15,7 @@ module Normalize
   class Normalize < Treetop::Runtime::SyntaxNode
     def to_s
       t=text_value
-      t=t.downcase
+      t=t.upcase
       t=t.gsub(/\t/,' ') # convert tabs to spaces
       t=t.gsub(/  */,'') # remove spaces
       t=t.gsub(/;/,"\n") # split semicolon-delimited statemtns
