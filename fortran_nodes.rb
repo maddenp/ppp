e380a13eb3160d97f4e127455e82a71e4886c964
Paul Madden <paul.a.madden@noaa.gov>
2012-06-06 15:21:11 -0600

    more hierarchy in fortran_nodes.rb classes

--- fortran_nodes.rb
+++ fortran_nodes.rb
@@ -1,38 +1,51 @@
 module Fortran
 
-  class Program < Treetop::Runtime::SyntaxNode
+  class ASTNode < Treetop::Runtime::SyntaxNode
+    def to_s
+      "### undefined to_s, please fix ###"
+    end
+  end
+
+  class Program < ASTNode
     def to_s
       elements.join
     end
   end
 
-  class Program_End_1 < Treetop::Runtime::SyntaxNode
+  class Program_End < ASTNode
+  end
+
+  class Program_End_1 < Program_End
     def to_s
       program_name=elements[1].text_value.downcase
       "end program #{program_name}\n"
     end
   end
 
-  class Program_End_2 < Treetop::Runtime::SyntaxNode
+  class Program_End_2 < Program_End
     def to_s
       "end program\n"
     end
   end
 
-  class Program_End_3 < Treetop::Runtime::SyntaxNode
+  class Program_End_3 < Program_End
     def to_s
       "end\n"
     end
   end
 
-  class Program_Start_1 < Treetop::Runtime::SyntaxNode
+
+  class Program_Start < ASTNode
+  end
+
+  class Program_Start_1 < Program_Start
     def to_s
       program_name=elements[1].text_value.downcase
       "program #{program_name}\n"
     end
   end
 
-  class Program_Start_2 < Treetop::Runtime::SyntaxNode
+  class Program_Start_2 < Program_Start
     def to_s
       ""
     end
