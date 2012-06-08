a6dcc6bce7feae383a6baed0ad4033ea150b7de5
Paul Madden <paul.a.madden@noaa.gov>
2012-06-08 10:00:06 -0600

    one-line node methods; factor program_name out into method

--- fortran_nodes.rb
+++ fortran_nodes.rb
@@ -1,37 +1,27 @@
 module Fortran
 
   class ASTNode < Treetop::Runtime::SyntaxNode
-    def to_s
-      "### undefined to_s, please fix ###"
-    end
+    def to_s() "### undefined to_s, please fix ###" end
   end
 
   class Program < ASTNode
-    def to_s
-      elements.join
-    end
+    def to_s() elements.join end
   end
 
   class Program_End < ASTNode
   end
 
   class Program_End_1 < Program_End
-    def to_s
-      program_name=elements[1].text_value.downcase
-      "end program #{program_name}\n"
-    end
+    def program_name() elements[1].text_value.downcase end
+    def to_s() "end program #{program_name}\n" end
   end
 
   class Program_End_2 < Program_End
-    def to_s
-      "end program\n"
-    end
+    def to_s() "end program\n" end
   end
 
   class Program_End_3 < Program_End
-    def to_s
-      "end\n"
-    end
+    def to_s() "end\n" end
   end
 
 
@@ -39,16 +29,12 @@ module Fortran
   end
 
   class Program_Start_1 < Program_Start
-    def to_s
-      program_name=elements[1].text_value.downcase
-      "program #{program_name}\n"
-    end
+    def program_name() elements[1].text_value.downcase end
+    def to_s() "program #{program_name}\n" end
   end
 
   class Program_Start_2 < Program_Start
-    def to_s
-      ""
-    end
+    def to_s() "" end
   end
 
 end
