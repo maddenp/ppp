177b82051cc0e0106d5d5150137a9473a1cf53af
Paul Madden <paul.a.madden@noaa.gov>
2012-06-26 15:51:25 -0600

    simplify end_program_stmt

--- fortran_nodes.rb
+++ fortran_nodes.rb
@@ -1,5 +1,9 @@
 module Fortran
 
+  class Treetop::Runtime::SyntaxNode
+    def to_s() '' end
+  end
+
   class ASTNode < Treetop::Runtime::SyntaxNode
     def initialize(a='',b=(0..0),c=[]) super(a,b,c) end
     def get(k) (@attrs.nil?)?(nil):(@attrs[k]) end
@@ -12,24 +16,16 @@ module Fortran
   end
 
   class End_Program_Stmt < ASTNode
-    def program_name() '' end
-  end
-
-  class End_Program_Stmt_0 < End_Program_Stmt
-    def program_name() elements[1].text_value.downcase end
-    def to_s() "end program #{program_name}\n" end
-  end
-
-  class End_Program_Stmt_1 < End_Program_Stmt
-    def to_s() "end program\n" end
-  end
-
-  class End_Program_Stmt_2 < End_Program_Stmt
-    def to_s() "end\n" end
+    def program_name() elements[2] end
+    def to_s() elements.join end
   end
 
   class Program_Stmt < ASTNode
     def program_name() '' end
+  end
+
+  class Keyword < ASTNode
+    def to_s() text_value end
   end
 
   class Program_Stmt_0 < Program_Stmt
