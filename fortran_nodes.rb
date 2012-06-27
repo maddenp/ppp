4d4b2d67d833a9b2469e04d73932503a2e6a1511
Paul Madden <paul.a.madden@noaa.gov>
2012-06-27 11:21:06 -0600

    code formatting

--- fortran_nodes.rb
+++ fortran_nodes.rb
@@ -7,14 +7,14 @@ module Fortran
   end
 
   class ASTNode < Treetop::Runtime::SyntaxNode
-    def cat()      elements.join                                   end
-    def get(k)     (@attrs.nil?)?(nil):(@attrs[k])                 end
-    def initialize (a='',b=(0..0),c=[]) super(a,b,c)               end
-    def join()     elements.join(' ')                              end
-    def method_missing(m) (m=~/e(\d+)/)?(elements[$~[1]]):()       end
-    def set(k,v)   (@attrs.nil?)?(@attrs={k=>v}):(@attrs[k]=v)     end
-    def to_s()     "!!! #{this.class} has no to_s, please fix"     end
-    def verbatim() text_value                                      end
+    def cat()             elements.join                               end
+    def get(k)            (@attrs.nil?)?(nil):(@attrs[k])             end
+    def initialize        (a='',b=(0..0),c=[]) super(a,b,c)           end
+    def join()            elements.join(' ')                          end
+    def method_missing(m) (m=~/e(\d+)/)?(elements[$~[1]]):()          end
+    def set(k,v)          (@attrs.nil?)?(@attrs={k=>v}):(@attrs[k]=v) end
+    def to_s()            "!!! #{this.class} has no to_s, please fix" end
+    def verbatim()        text_value                                  end
   end
 
   class Main_Program < ASTNode
