32ffe5e76ea08f5694dc4d68e0b943006d7f9aaa
Paul Madden <paul.a.madden@noaa.gov>
2012-06-27 10:41:21 -0600

    simplify program_stmt

--- fortran_nodes.rb
+++ fortran_nodes.rb
@@ -1,14 +1,18 @@
 module Fortran
 
+  # TODO ASTNode's to_s() should do elements.join as default?
+
   class Treetop::Runtime::SyntaxNode
     def to_s() '' end
   end
 
   class ASTNode < Treetop::Runtime::SyntaxNode
-    def initialize(a='',b=(0..0),c=[]) super(a,b,c) end
-    def get(k) (@attrs.nil?)?(nil):(@attrs[k]) end
-    def set(k,v) (@attrs.nil?)?(@attrs={k=>v}):(@attrs[k]=v) end
-    def to_s() "### #{this.class} has no to_s, please fix ###" end
+    def initialize (a='',b=(0..0),c=[]) super(a,b,c)               end
+    def get(k)     (@attrs.nil?)?(nil):(@attrs[k])                 end
+    def out()      elements.join(' ')                              end
+    def set(k,v)   (@attrs.nil?)?(@attrs={k=>v}):(@attrs[k]=v)     end
+    def to_s()     "!!! #{this.class} has no to_s, please fix"     end
+    def verbatim() text_value                                      end
   end
 
   class Main_Program < ASTNode
@@ -16,25 +20,21 @@ module Fortran
   end
 
   class End_Program_Stmt < ASTNode
-    def program_name() elements[2] end
-    def to_s() elements.join end
+    def name() elements[2] end
+    def to_s() out end
+  end
+
+  class Name < ASTNode
+    def to_s() verbatim end
   end
 
   class Program_Stmt < ASTNode
-    def program_name() '' end
+    def name() elements[1] end
+    def to_s() out end
   end
 
-  class Keyword < ASTNode
-    def to_s() text_value end
-  end
-
-  class Program_Stmt_0 < Program_Stmt
-    def program_name() elements[1].text_value.downcase end
-    def to_s() "program #{program_name}\n" end
-  end
-
-  class Program_Stmt_1 < Program_Stmt
-    def to_s() "" end
+  class Terminal < ASTNode
+    def to_s() verbatim end
   end
 
 end
