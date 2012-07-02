9c142cea08f17d10e1f6fa9f1e68fc63e65d68de
Paul Madden <paul.a.madden@noaa.gov>
2012-07-02 13:23:04 -0600

    block[begin|end] instead of [un]indent

--- fortran_nodes.rb
+++ fortran_nodes.rb
@@ -8,16 +8,17 @@ module Fortran
 
   class ASTNode < Treetop::Runtime::SyntaxNode
     @@level=0
+    def blockend(s) @@level-=1 unless @@level==0; s.sub(/^  /,'') end
+    def blockbegin(s) @@level+=1; s end
     def cat() elements.join end
     def get(k) (@attrs.nil?)?(nil):(@attrs[k]) end
-    def indent(s) @@level+=1; s end
+    def indent(s) '  '*@@level+s end
     def initialize(a='',b=(0..0),c=[]) super(a,b,c) end
     def join() elements.join(' ') end
     def method_missing(m) (m=~/e(\d+)/)?(elements[$~[1]]):() end
     def set(k,v) (@attrs.nil?)?(@attrs={k=>v}):(@attrs[k]=v) end
-    def stmt(s) '  '*@@level+s end
+    def stmt(s) indent(s) end
     def to_s() "!!! #{this.class} has no to_s, please fix" end
-    def unindent(s) @@level-=1 unless @@level==0; s.sub(/^  /,'') end
     def verbatim() text_value end
   end
 
@@ -27,7 +28,7 @@ module Fortran
 
   class End_Program_Stmt < ASTNode
     def name() e2 end
-    def to_s() unindent(stmt(join)) end
+    def to_s() blockend(stmt(join)) end
   end
 
   class Name < ASTNode
@@ -40,7 +41,7 @@ module Fortran
 
   class Program_Stmt < ASTNode
     def name() e1 end
-    def to_s() indent(stmt(join)) end
+    def to_s() blockbegin(stmt(join)) end
   end
 
   class Verbatim < ASTNode
