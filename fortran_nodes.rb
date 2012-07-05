b2d9edef3f798834711febca6b019015e11c92c7
Paul Madden <paul.a.madden@noaa.gov>
2012-07-05 10:50:42 -0600

    simple 'print *' test passes

--- fortran_nodes.rb
+++ fortran_nodes.rb
@@ -10,25 +10,25 @@ module Fortran
     @@level=0
     def blockend(s) @@level-=1 unless @@level==0; s.sub(/^  /,'') end
     def blockbegin(s) @@level+=1; s end
-    def cat() elements.join end
+    def cat() elements.map { |e| e.to_s }.join end
     def get(k) (@attrs.nil?)?(nil):(@attrs[k]) end
     def indent(s) '  '*@@level+s end
     def initialize(a='',b=(0..0),c=[]) super(a,b,c) end
-    def join() elements.join(' ') end
-    def method_missing(m) (m=~/e(\d+)/)?(elements[$~[1]]):() end
+    def join() elements.map { |e| e.to_s }.join(' ') end
+    def method_missing(m) (m=~/e(\d+)/)?(elements[$~[1].to_i]):('') end
     def set(k,v) (@attrs.nil?)?(@attrs={k=>v}):(@attrs[k]=v) end
     def stmt(s) indent(s) end
     def to_s() "!!! #{this.class} has no to_s, please fix" end
     def verbatim() text_value end
   end
 
-  class Main_Program < ASTNode
-    def to_s() cat end
-  end
-
   class End_Program_Stmt < ASTNode
     def name() e2 end
     def to_s() blockend(stmt(join)) end
+  end
+
+  class Main_Program < ASTNode
+    def to_s() cat end
   end
 
   class Name < ASTNode
