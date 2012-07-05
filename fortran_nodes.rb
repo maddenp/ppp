0d4490cae0c6d13189164eb9b60e72ab0da2dba7
Paul Madden <paul.a.madden@noaa.gov>
2012-07-05 11:50:58 -0600

    expr ::= level_2_expr

--- fortran_nodes.rb
+++ fortran_nodes.rb
@@ -7,19 +7,59 @@ module Fortran
   end
 
   class ASTNode < Treetop::Runtime::SyntaxNode
+
     @@level=0
-    def blockend(s) @@level-=1 unless @@level==0; s.sub(/^  /,'') end
-    def blockbegin(s) @@level+=1; s end
-    def cat() elements.map { |e| e.to_s }.join end
-    def get(k) (@attrs.nil?)?(nil):(@attrs[k]) end
-    def indent(s) '  '*@@level+s end
-    def initialize(a='',b=(0..0),c=[]) super(a,b,c) end
-    def join() elements.map { |e| e.to_s }.join(' ') end
-    def method_missing(m) (m=~/e(\d+)/)?(elements[$~[1].to_i]):('') end
-    def set(k,v) (@attrs.nil?)?(@attrs={k=>v}):(@attrs[k]=v) end
-    def stmt(s) indent(s.chomp)+"\n" end
-    def to_s() "!!! #{this.class} has no to_s, please fix" end
-    def verbatim() text_value end
+
+    def blockbegin(s)
+      @@level+=1
+      s
+    end
+
+    def blockend(s)
+      @@level-=1 unless @@level==0
+      s.sub(/^  /,'')
+    end
+
+    def cat()
+      elements.map { |e| e.to_s }.join
+    end
+
+    def get(k)
+      (@attrs.nil?)?(nil):(@attrs[k])
+    end
+
+    def indent(s)
+      '  '*@@level+s
+    end
+
+    def initialize(a='',b=(0..0),c=[])
+      super(a,b,c)
+    end
+
+    def join()
+      elements.map { |e| e.to_s }.join(' ')
+    end
+
+    def method_missing(m)
+      (m=~/e(\d+)/)?(elements[$~[1].to_i]):('')
+    end
+
+    def set(k,v)
+      (@attrs.nil?)?(@attrs={k=>v}):(@attrs[k]=v)
+    end
+
+    def stmt(s)
+      indent(s.chomp)+"\n"
+    end
+
+    def to_s()
+      "!!! #{this.class} has no to_s, please fix"
+    end
+
+    def verbatim()
+      text_value
+    end
+
   end
 
   class End_Program_Stmt < ASTNode
