dbcb7aead6c3827959df8e10277b449404951740
Paul Madden <paul.a.madden@noaa.gov>
2012-07-10 14:28:55 -0600

    rel_op test

---
+++ tests/t0060/t.f90
@@ -0,0 +1,12 @@
+program p
+  print *,1.0.gt.0.1
+  print *,1.0> 0.1
+  print *,1.lt.2
+  print *,1<2
+  print *,1.ge.1.0
+  print *,1.le.1.0
+  print *,1.ne.2
+  print *,1/=2
+  print *,1==1.0
+  print *,1.eq.1.0
+end program p
