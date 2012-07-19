97c66e0150822ee0cc7794ce685337beb7ab19a2
Paul Madden <paul.a.madden@noaa.gov>
2012-07-19 15:26:55 -0600

    another type_declaration_stmt test

---
+++ tests/t0109/t.f90
@@ -0,0 +1,12 @@
+program p
+  real::a=2.71
+  real(kind=8)::b=3.14
+  double precision::c=7.77
+  complex::d=(2.71,3.14)
+  logical::e=.true.
+  print *,a
+  print *,b
+  print *,c
+  print *,d
+  print *,e
+end program p
