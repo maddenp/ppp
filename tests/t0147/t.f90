ee20ca35fdecf0a010e126a52fe8f72078738394
Paul Madden <paul.a.madden@noaa.gov>
2012-07-26 14:49:06 -0600

    test for data_i_do_object with scalar_structure_component

---
+++ tests/t0147/t.f90
@@ -0,0 +1,13 @@
+program t
+  implicit none
+  type x
+    integer::a
+  end type x
+  type y
+    type(x)::b(3)
+  end type y
+  type(y)::z
+  integer::i
+  data (z%b(i)%a,i=1,3) /1,2,3/
+  print '(i0)',z%b
+end program t
