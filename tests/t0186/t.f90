8a36b56eb45c9f889ef48ca7b16ae7c7741eb572
Paul Madden <paul.a.madden@noaa.gov>
2012-08-01 16:53:17 -0600

    endfile_stmt + tests

---
+++ tests/t0186/t.f90
@@ -0,0 +1,21 @@
+program t
+  integer::ierr
+  character(5)::a='*****'
+  character(5)::b='*****'
+100 format (i0)
+  open(unit=77,file='tmpfile',status='new',iostat=ierr)
+  write(77,fmt='(a)') 'hello'
+  write(77,fmt='(a)') 'world'
+  close(77,iostat=ierr)
+  open(unit=77,file='tmpfile',status='old',iostat=ierr)
+  read(77,fmt='(a)',iostat=ierr) a
+  print '(a)',a
+  endfile 77
+  read(77,fmt='(a)',iostat=ierr) b
+  if (ierr.eq.0) then
+    stop 'read past eof'
+  else
+    print '(a)','ok'
+  endif
+  close(77,iostat=ierr)
+end program t
