517512d9079c2f764581d68a6376fbd2a1851499
Paul Madden <paul.a.madden@noaa.gov>
2012-08-03 17:28:13 -0600

    external_subprogram -- needs test(s)

--- normalize_nodes.rb
+++ normalize_nodes.rb
@@ -15,35 +15,11 @@ module Normalize
   class Normalize < Treetop::Runtime::SyntaxNode
     def to_s
       t=text_value
-      # Basic transformations: Per [std:f90:3.1.1], lowercase is optional, so
-      # use uppercase internally; Convert tabs to spaces for convenience; Per
-      # [std:f90:3.3.1], multiple spaces are treated as one, so squeeze to
-      # single space for convenience; Split semicolon-delimited lines
-      # [std:f90:3.3.1.2].
       t=t.downcase
-      t=t.gsub(/\t/,' ')
-#     t=t.gsub(/  */,' ') # spaces: single
-      t=t.gsub(/  */,'')  # spaces: none
-      t=t.gsub(/;/,"\n")
-#     # Remove optional spaces from keywords [std:f90:3.3.1].
-#     t=t.gsub(/block data/,'blockdata')
-#     t=t.gsub(/double precision/,'doubleprecision')
-#     t=t.gsub(/else if/,'elseif')
-#     t=t.gsub(/end blockdata/,'endblockdata')
-#     t=t.gsub(/end do/,'enddo')
-#     t=t.gsub(/end file/,'endfile')
-#     t=t.gsub(/end function/,'endfunction')
-#     t=t.gsub(/end if/,'endif')
-#     t=t.gsub(/end interface/,'endinterface')
-#     t=t.gsub(/end module/,'endmodule')
-#     t=t.gsub(/end program/,'endprogram')
-#     t=t.gsub(/end select/,'endselect')
-#     t=t.gsub(/end subroutine/,'endsubroutine')
-#     t=t.gsub(/end type/,'endtype')
-#     t=t.gsub(/end where/,'endwhere')
-#     t=t.gsub(/go to/,'goto')
-#     t=t.gsub(/in out/,'inout')
-#     t=t.gsub(/select case/,'selectcase')
+      t=t.gsub(/\t/,' ')  # convert tabs to spaces
+#     t=t.gsub(/  */,' ') # squeeze spaces
+      t=t.gsub(/  */,'')  # remove spaces
+      t=t.gsub(/;/,"\n")  # split semicolon-delimited statement lines
     end
   end
 
