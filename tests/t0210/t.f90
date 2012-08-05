b9e5e87c861945053d74f29976c0480adfa65035
Paul Madden <maddenp_at_colorado_dot_edu>
2012-08-05 17:19:58 -0600

    line-continuation test

---
+++ tests/t0210/t.f90
@@ -0,0 +1,22 @@
+PROGRAM asdf
+  ! comment 1
+  print '(a)', & ! comment 2
+'hello world 1'
+  print '(a)','heLLo "x" worLd 2'     
+  print '(a)','heLLo ''x'' worLd 3' ! comment 3
+7 PRINT '(a)',"heLLo 'x' worLd 4" ! don't continue &
+  PRINT '(a)',"heLLo ""x"" worLd 5"
+  8 PRINT '(a)',"heLLo ""x"" &
+worLd 6"
+  PRINT '(a)',"heLLo ""x"" &
+
+! comment 4
+   worLd 7"
+  PRINT '(a)',"heLLo ""x"" &
+    &     worLd 8"
+  print '(a)','hello' ;;
+
+  continue ;;; print '(a)','world 9'
+  print '(a)','hello' ; &
+    print '(a)','world 0'
+end prOGRam ASDF
