1b3331b84e95a50637684dc05226afc59bbfa622
Paul Madden <paul.a.madden@noaa.gov>
2012-09-20 11:37:06 -0600

    removed unneeded misc files (have been incorporated into tests)

--- misc/incdir/i"c.f90
+++
@@ -1,6 +0,0 @@
-subroutine q
-  implicit none
-  integer::i=5
-  print *,i
-  include 'i2.f90'
-end subroutine q
--- misc/longline.f90
+++
@@ -1,4 +0,0 @@
-program p
-  print '(a)','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed &
-    &do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-end program p
