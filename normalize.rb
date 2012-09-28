688bd3e9a115d34f7b6709d8590b5f66d87a55b1
Paul Madden <paul.a.madden@noaa.gov>
2012-09-28 14:23:06 -0600

    hack to normalize *some* sms directives

--- normalize.rb
+++ normalize.rb
@@ -59,9 +59,14 @@ module Normalize
   class SMS < Treetop::Runtime::SyntaxNode
     def to_s
       t=text_value
-      t=t.gsub(/^\s+/,'')   # left-justify lines
-      t=t.downcase          # make upper-case characters lower-case
-      t=t.gsub(/[ \t]+/,'') # remove tabs & spaces
+      t=t.gsub(/^\s+/,'') # left-justify lines
+      # REMOVE this HACK when all sms directives are being translated
+      re=Regexp.new('@sms\$(barrier|ignore)')
+      if re.match(t)
+        t=t.downcase          # make upper-case characters lower-case
+        t=t.gsub(/[ \t]+/,'') # remove tabs & spaces
+      end
+      t
     end
   end
 
