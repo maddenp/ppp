9cc87a12cb5e675c499659d8f4d17e0c5635463b
Paul Madden <maddenp_at_colorado_dot_edu>
2012-10-03 17:40:18 -0600

    expose H format descriptor stuff in Normalize

--- normalize.rb
+++ normalize.rb
@@ -28,23 +28,23 @@ module Normalize
       a=t.split("\n")
       a.each_index do |i|
         l=a[i]
-#       h=false
-#       p="\(.*?[0-9]{1,5}[ \t]format[ \t]*\\(.*?\)\([0-9]+\)[ \t]*[hH]\(.*?\)\\)\(.*\)"
-#       r=Regexp.new(p,Regexp::IGNORECASE|Regexp::MULTILINE)
-#       while m=r.match(l)
-#         h=true
-#         p1=m[3][0..m[2].to_i-1]
-#         p2=m[3].sub(/^#{p1}/,'')
-#         l="#{m[1]}'#{p1}'#{p2})#{m[4]}"
-#       end
+        h=false
+        p="\(.*?[0-9]{1,5}[ \t]format[ \t]*\\(.*?\)\([0-9]+\)[ \t]*[hH]\(.*?\)\\)\(.*\)"
+        r=Regexp.new(p,true)
+        while m=r.match(l)
+          h=true
+          p1=m[3][0..m[2].to_i-1]
+          p2=m[3].sub(/^#{p1}/,'')
+          l="#{m[1]}'#{p1}'#{p2})#{m[4]}"
+        end
         # If a F90:1016 conversion occurred, quoted strings have been introduced
         # and it is no longer safe to change case or whitespace. Note, though,
         # that these conversions can only happen in the first normalization pass,
         # so that the second pass can normalize case and whitespace.
-#       unless h
+        unless h
           l=l.downcase # make upper-case characters lower-case
           l=l.gsub(/[ \t]+/,'') # remove tabs & spaces
-#       end
+        end
         a[i]=l
       end
       t=a.join("\n")
