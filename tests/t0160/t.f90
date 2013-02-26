2f94874804d073faebbdc7e20c4b8e5d785d88eb
Paul Madden <paul.a.madden@noaa.gov>
2013-02-26 15:35:17 -0700

    update t0160 to actually use arrays, to verify no syntax is elided

--- tests/t0160/t.f90
+++ tests/t0160/t.f90
@@ -1,6 +1,12 @@
 program t
   integer::i,j,k
-  target::i(3)
-  target j(:,:),k(3,3)
+  target::i(1)
+  target j(:,:),k(4,5)
   allocatable::j
+  allocate(j(2,3))
+  print '(i0)',size(i,1)
+  print '(i0)',size(j,1)
+  print '(i0)',size(j,2)
+  print '(i0)',size(k,1)
+  print '(i0)',size(k,2)
 end program t
