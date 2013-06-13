47db21c76db1df0a0abf8d0ac2ff0c2b5dff4118
Matthew Hurst <matthew.hurst@noaa.gov>
2013-06-13 16:32:07 -0600

    added test for format specifier in R912

---
+++ tests/t0298/t.f90
@@ -0,0 +1,22 @@
+program t1
+  implicit none
+  integer::a
+  real::b
+  character*4::c
+  logical::d
+  open (90, file='tmpfile', status='new')
+  write (90, '(i5)') 1043
+  write (90, '(f4.2)') 3.141592654
+  write (90, '(a7)') 'hello'
+  write (90, '(l)') .true.
+  rewind (90)
+  read (90, '(i6)') a
+  print *,a
+  read (90, '(f5.1)') b
+  print *,b
+  read (90, '(a4)') c
+  print *,c
+  read (90, '(l)') d
+  print *,d
+endprogram t1
+
