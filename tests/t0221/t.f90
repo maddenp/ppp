18f857eb15c92e059fa46de4ffe0d8be6f621b5d
Matthew Hurst <matthew.hurst@noaa.gov>
2013-06-19 14:38:47 -0600

    updated t0221 to include generic assignment operator

--- tests/t0221/t.f90
+++ tests/t0221/t.f90
@@ -5,15 +5,24 @@ module m
     integer::y
   end type vector
   interface operator (.plus.)
-     module procedure addw
-  end interface operator (.plus.)
+     module procedure addvec
+   end interface operator (.plus.)
+   interface assignment (=)
+     module procedure assignvec
+   endinterface assignment (=)
 contains
-  function addw(a,b)
+  function addvec(a,b)
     type(vector),intent(in)::a,b
-    type(vector)::addw
-    addw%x=a%x+b%x
-    addw%y=a%y+b%y
-  end function addw
+    type(vector)::addvec
+    addvec%x=a%x+b%x
+    addvec%y=a%y+b%y
+  end function addvec
+  subroutine assignvec (b,a)
+    type(vector),intent(in)::a
+    real,intent(out)::b
+    b = sqrt((a%x**2.0) + (a%y**2.0))
+  endsubroutine assignvec
+    
 end module m
 
 program t
@@ -21,6 +30,7 @@ program t
   implicit none
   type(vector)::f,g
   integer::h,i
+  real::magnitude
   f%x=7
   f%y=8
   g%x=9
@@ -29,4 +39,6 @@ program t
   i=8
   print *,f.plus.g
   print *,h+i
+  magnitude =f
+  print *,magnitude
 end program t
