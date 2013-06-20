3b44b653630cc7c15a3b21bff7cd2124fa5706c3
Paul Madden <paul.a.madden@noaa.gov>
2013-06-20 09:07:46 -0600

    improve t0256 (openmp directives) and add support to common makefile

--- tests/t0256/t.f90
+++ tests/t0256/t.f90
@@ -1,4 +1,11 @@
 program t
-!$OMP CRITICAL
-!$OMP END CRITICAL
+  implicit none
+! use omp_lib
+  integer::id,nthreads,omp_get_thread_num
+!$omp parallel private(nthreads,id)
+  id=omp_get_thread_num()
+  if (id.eq.1) then
+    print *,'second thread'
+  end if
+!$omp end parallel
 end program t
