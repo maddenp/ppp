9638b7db7ebe7eab92f0ba6e7ea889964a688088
Paul Madden <paul.a.madden@noaa.gov>
2013-06-26 17:58:06 -0600

    factor out commonalities in norm[fixed|free].rb

--- normfixed.rb
+++ normfixed.rb
@@ -1,48 +1,15 @@
+require "normcommon"
+
+include Normcommon
+
 module Normfixed
-
-  class Text < Treetop::Runtime::SyntaxNode
-    def to_s
-      elements.reduce("") { |s,e| s+="#{e}" }
-    end
-  end
-
-  class Delete < Treetop::Runtime::SyntaxNode
-    def to_s
-      ""
-    end
-  end
-
-  class Directive < Treetop::Runtime::SyntaxNode
-    def to_s
-      t=text_value
-    end
-  end
 
   class Normalize < Treetop::Runtime::SyntaxNode
     def to_s
       t=text_value
-      # Join continuation lines
-      t=t.gsub(/$\n[ \t]{5}[^0 \t]/,"")
-      # Attempting a multiline match of the regular expression below on a string
-      # representing a huge source file is incredibly slow. Breaking the string
-      # into lines improves performance significantly. 
-      a=t.split("\n")
-      a.each_index do |i|
-        l=a[i]
-        # Convert instances of F90:1016 char-string-edit-desc to quoted strings
-        # to preserve case and whitespace.
-        p="\(.*?[0-9]{1,5}[ \t]*format[ \t]*\\(.*?\)\([0-9]+\)[ \t]*[hH]\(.*?\)\\)\(.*\)"
-        r=Regexp.new(p,true)
-        while m=r.match(l)
-          p1=m[3][0..m[2].to_i-1]
-          p2=m[3].sub(/^#{p1}/,"")
-          l="#{m[1]}'#{p1}'#{p2})#{m[4]}"
-        end
-        a[i]=l
-      end
-      t=a.join("\n")
-      # Remove blank lines
-      t=t.gsub(/\n\n+/,"\n")
+      t=t.gsub(/$\n[ \t]{5}[^0 \t]/,"") # join continuation lines
+      t=fix_h_edit_descriptor(t)        # fix F90:1016 'h' edit descriptors
+      t=t.gsub(/\n\n+/,"\n")            # remove blank lines
     end
   end
 
--- normfree.rb
+++ normfree.rb
@@ -1,72 +1,25 @@
+require "normcommon"
+
+include Normcommon
+
 module Normfree
-
-  class Text < Treetop::Runtime::SyntaxNode
-    def to_s
-      elements.reduce("") { |s,e| s+="#{e}" }
-    end
-  end
-
-  class Delete < Treetop::Runtime::SyntaxNode
-    def to_s
-      ""
-    end
-  end
-
-  class Directive < Treetop::Runtime::SyntaxNode
-    def to_s
-      t=text_value
-      t="\n"+t
-    end
-  end
 
   class Normalize < Treetop::Runtime::SyntaxNode
     def to_s
       t=text_value
-      # Join continuation lines
-      t=t.gsub(/&$(\n&?)?/,"")
-      # Split semicolon-delimited statement lines
-      t=t.gsub(/\s*;\s*/,"\n")
-      # Attempting a multiline match of the regular expression below on a string
-      # representing a huge source file is incredibly slow. Breaking the string
-      # into lines improves performance significantly. 
-      a=t.split("\n")
-      a.each_index do |i|
-        l=a[i]
-        # Convert instances of F90:1016 char-string-edit-desc to quoted strings
-        # to preserve case and whitespace.
-        h=false
-        p="\(.*?[0-9]{1,5}[ \t]*format[ \t]*\\(.*?\)\([0-9]+\)[ \t]*[hH]\(.*?\)\\)\(.*\)"
-        r=Regexp.new(p,true)
-        while m=r.match(l)
-          h=true
-          p1=m[3][0..m[2].to_i-1]
-          p2=m[3].sub(/^#{p1}/,"")
-          l="#{m[1]}'#{p1}'#{p2})#{m[4]}"
-        end
-        # If a F90:1016 conversion occurred, quoted strings have been introduced
-        # and it is no longer safe to change case or whitespace. Note, though,
-        # that these conversions can only happen in the first normalization pass,
-        # so that the second pass can normalize case and whitespace.
-        unless h
-          # Make upper-case characters lower-case
-          l=l.downcase
-          # Remove tabs & spaces
-          l=l.gsub(/[ \t]+/,"")
-        end
-        a[i]=l
-      end
-      t=a.join("\n")
-      # Remove blank lines
-      t=t.gsub(/\n\n+/,"\n")
+      t=t.gsub(/&$(\n&?)?/,"")   # join continuation lines
+      t=t.gsub(/\s*;\s*/,"\n")   # split semicolon-delimited statement lines
+      t=fix_h_edit_descriptor(t) # fix F90:1016 'h' edit descriptors
+      t=t.gsub(/\n\n+/,"\n")     # remove blank lines
     end
   end
 
   class Quoted < Treetop::Runtime::SyntaxNode
     def to_s
       t=text_value
-      t=t.gsub(/^!.*/,"")    # no comment lines
-      t=t.gsub(/\n\n+/,"\n") # no blank lines
-      t=t.gsub(/&$\n&?/,"")  # join continuation lines
+      t=t.gsub(/^!.*/,"")        # remove comment lines
+      t=t.gsub(/\n\n+/,"\n")     # remove blank lines
+      t=t.gsub(/&$\n&?/,"")      # join continuation lines
     end
   end
 
