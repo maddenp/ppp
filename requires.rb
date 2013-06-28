53edc15ca89de3a56f5df5ba0733a982ef3ca66f
Paul Madden <paul.a.madden@noaa.gov>
2013-06-28 08:09:38 -0600

    update requires.rb

--- requires.rb
+++ requires.rb
@@ -16,17 +16,17 @@ require "treetop/runtime"
 
 require "normfixed"
 require "normfixed_parser"
-require "sms_normfixed_parser"
 require "sms_normfixed"
+require "sms_normfixed_parser"
 
 require "normfree"
 require "normfree_parser"
-require "sms_normfree_parser"
 require "sms_normfree"
+require "sms_normfree_parser"
 
 require "fortran"
 require "fortran_parser"
-require "sms_fortran_parser"
 require "sms_fortran"
+require "sms_fortran_parser"
 
 require "translator"
