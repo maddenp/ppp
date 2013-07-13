e7133c7dbdbf7b4717a3de7f2600edf53f20c84e
Paul Madden <maddenp_at_colorado_dot_edu>
2013-07-12 21:10:39 -0600

    add tests for string literals in format stmts with h edit-descriptors

---
+++ tests/t0329x/t.f
@@ -0,0 +1,11 @@
+      program t
+11111 FORMAT (5hHello) ; print 11111
+22222 FORMAT (11hHello World) ; print 22222
+33333 FORMAT ('Hello ',5hWorld) ; print 33333
+44444 FORMAT ('Hello ', ! comment
+     *5hWorld) ; print 44444
+55555 FORMAT ('Hello ',
+     &5hWorld) ; print 55555
+66666 FORMAT ('Hello ', ! comment
+     _5hWorld) ; print 66666
+      end program t
