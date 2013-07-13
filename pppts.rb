f12af147c44c8949bb44a2de8d8f5e089ae04a90
Paul Madden <maddenp_at_colorado_dot_edu>
2013-07-12 21:41:05 -0600

    restore two globals in pppts.rb -- t0256 tests now fail properly

--- pppts.rb
+++ pppts.rb
@@ -2,31 +2,31 @@ debug=false
 server_mode=true
 threads=8
 
-def pppts_exe(cmd,lock,failure)
+def pppts_exe(cmd)
   out=IO.popen(cmd+" 2>&1") { |e| e.readlines.reduce("") { |s,e| s+=e } }
-  pppts_fail(lock,failure,out,cmd) unless $?.exitstatus==0
+  pppts_fail(out,cmd) unless $?.exitstatus==0
   out
 end
 
-def pppts_fail(lock,failure,msg=nil,cmd=nil)
-  lock.synchronize do
-    return if failure
+def pppts_fail(msg=nil,cmd=nil)
+  @lock.synchronize do
+    return if @failure
     puts "\nFAIL"+((cmd)?(": #{cmd}\n"):(""))
     puts msg.lines.reduce("") { |m,e| m+="      #{e}" } if msg
-    failure=true
+    @failure=true
   end
 end
 
-def pppts_run_all(threads,socket,debug,failure,lock)
-  def go(q,socket,debug,failure,lock) 
-    pppts_test(q.deq,socket,debug,failure,lock) until q.empty?
+def pppts_run_all(threads,socket,debug)
+  def go(q,socket,debug) 
+    pppts_test(q.deq,socket,debug) until q.empty?
   end
   tdir="tests"
   q=Queue.new
   tests=(ARGV[0])?(["#{tdir}/#{ARGV[0]}"]):(Dir.glob("#{tdir}/t*").sort)
   tests.each { |e| q.enq(e) }
   runners=(1..threads).reduce([]) do |m,e|
-    m << Thread.new { go(q,socket,debug,failure,lock) }
+    m << Thread.new { go(q,socket,debug) }
   end
   runners.each { |e| e.join }
   tests.size
@@ -40,26 +40,26 @@ def pppts_server_start
   [s,socket]
 end
 
-def pppts_test(t,socket,debug,failure,lock)
-  lock.synchronize do
-    return if failure
+def pppts_test(t,socket,debug)
+  @lock.synchronize do
+    return if @failure
     print "trying #{File.basename(t)}..." if debug
   end
-  pppts_exe("make -C #{t} clean",lock,failure)
+  pppts_exe("make -C #{t} clean")
   x=(socket)?(" SOCKET=#{socket} "):(" ")
-  pppts_exe("make -C #{t}#{x}comp",lock,failure)
-  pppts_exe("make -C #{t} clean",lock,failure)
-  lock.synchronize { (debug)?(puts " ok"):(print ".") unless failure }
+  pppts_exe("make -C #{t}#{x}comp")
+  pppts_exe("make -C #{t} clean")
+  @lock.synchronize { (debug)?(puts " ok"):(print ".") unless @failure }
 end
 
 threads=1 if debug
-failure=false
-lock=Mutex.new
-pppts_fail(lock,failure,"Tests require at least one thread") unless threads>0
-pppts_exe("make",lock,failure)
+@failure=false
+@lock=Mutex.new
+pppts_fail("Tests require at least one thread") unless threads>0
+pppts_exe("make")
 load File.join(File.dirname($0),"requires.rb")
 srvr,socket=(server_mode)?(pppts_server_start):(nil)
-n=pppts_run_all(threads,socket,debug,failure,lock)
-puts "\nOK (#{n} tests)" unless failure
+n=pppts_run_all(threads,socket,debug)
+puts "\nOK (#{n} tests)" unless @failure
 if srvr then srvr.raise(Interrupt); srvr.join end
-exit((failure)?(0):(1))
+exit((@failure)?(0):(1))
