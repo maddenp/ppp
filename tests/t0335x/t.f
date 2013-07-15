b9e1fc62ddc8c4311c26e84a147d4e8099af6fe9
Paul Madden <paul.a.madden@noaa.gov>
2013-07-15 15:45:48 -0600

    add detabify tests

---
+++ tests/t0335x/t.f
@@ -0,0 +1,11 @@
+      program t
+! NOTE This file contains tab characters, which is the point of the test.
+! NOTE Use emacs M-x whitespace-mode or similar when editing.
+      print *,
+	1 1
+	print *,2
+c	this is a comment
+100	print *,3
+ 200	print *,4
+3 0 0	print *,5
+      end program t
