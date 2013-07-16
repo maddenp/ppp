b0eada1b8d93035badd75fef096cfb258b9d6baa
Paul Madden <paul.a.madden@noaa.gov>
2013-07-16 15:53:45 -0600

    update TODO; fortran.tt cosmetics; uncomment lines in t0343r/t.f90

--- tests/t0343r/t.f90
+++ tests/t0343r/t.f90
@@ -1,6 +1,6 @@
 program t
-! character*5::a='hello'
-! character*5::b='world'
+  character*5::a='hello'
+  character*5::b='world'
 1234 format(2(3X,a3))
-! write (*,1234) a,b
+  write (*,1234) a,b
 endprogram t
