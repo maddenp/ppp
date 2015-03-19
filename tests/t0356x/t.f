f19979f8f4c00faf6b754ef2d298216d5e7c882b
Paul Madden <maddenp@colorado.edu>
2015-03-19 20:00:02 +0000

    improve t0356 tests

--- tests/t0356x/t.f
+++ tests/t0356x/t.f
@@ -1,8 +1,8 @@
       program t
       implicit none
       integer,parameter::lun=6
-      write (6,'(a)') 'x'
+      flush 6
+      flush lun
       flush (6)
-      write (lun,'(a)') 'x'
       flush (lun)
       end program t
